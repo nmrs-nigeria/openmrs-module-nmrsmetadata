77e002691b5dd53378e8914530c5daed49f9727d
Adeyemicodes <aadegoke007@gmail.com>
2025-06-23 23:51:35 +0100

    compilation of validated O3 forms

---
+++ api/src/main/resources/poc_related/forms/forms/conceptId_n_FormPublishing.sql
@@ -0,0 +1,121 @@
+UPDATE `openmrs`.`concept` SET `datatype_id` = '3' WHERE (`concept_id` = '167197');
+UPDATE `openmrs`.`concept` SET `changed_by` = '1' WHERE (`concept_id` = '164971');
+
+
+-- PUBLISHING THE FORMS
+
+-- TB Index Patient Contact Investigation Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '95');
+
+-- recency
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '75');
+
+-- ART commencement
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '56');
+
+-- child birth reg
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '19');
+
+-- antenal care details
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '45');
+
+-- Child Follow Up
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '8');
+
+-- Client Intake Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '8');
+
+-- Covid19 Contact Investigation Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '80');
+
+-- Delivery Register
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '15');
+
+-- DR-TB In-Patient Discharge
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '91');
+
+-- DR-TB Treatment Register
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '99');
+
+-- HIV Enrollment Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '23');
+
+-- IPT
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '53');
+
+-- Laboratory Order and Result Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '21');
+
+-- LGA-Health Facility TB Central Register Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '90');
+
+-- Maternal Cohort Register
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '48');
+
+-- OVC Linkage Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '108');
+
+-- Partner Register
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '40');
+
+-- Peadiatric Initial Clinical Evaluation Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '20');
+
+-- PMTCT HTS Register
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '54');
+
+-- PMTCT Registration Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '83');
+
+-- Presumptive TB Register Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '96');
+
+-- Specimen Examination Result Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '89');
+
+-- TB Patient Referral or Transfer Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '88');
+
+-- TB Patient Treatment Card Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '92');
+
+-- TB Referral For Community and Facility
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '93');
+
+-- TB Treatment Monitoring Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '106');
+
+-- TB Screening Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '102');
+
+-- TB Interruption Tracking Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '85');
+
+-- Covid19 Case Investigation Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '79');
+
+-- Client Referral Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '52');
+
+-- DR-TB In-Patient Discharge Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '91');
+
+-- General Antenatal Care
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '16');
+
+-- Risk Stratification Adult
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '76');
+
+-- Specimen Examination Request Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '94');
+
+-- Covid19 case Identification Form
+UPDATE `openmrs`.`form` SET `published` = '1' WHERE (`form_id` = '79');
+
+
+
+
+
+
+
+
