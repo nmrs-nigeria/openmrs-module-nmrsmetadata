9273b9c2d7a63af5e61042f1f62558456b42c70c
Toyeeb <abdulfataitoyeeb@gmail.com>
2026-05-02 14:54:37 +0100

    fix

--- api/src/main/resources/poc_related/new_tools/concepts/concept_answer.sql
+++ api/src/main/resources/poc_related/new_tools/concepts/concept_answer.sql
@@ -5,4 +5,8 @@ REPLACE INTO `concept_answer` (`concept_answer_id`,`concept_id`,`answer_concept`
 REPLACE INTO `concept_answer` (`concept_answer_id`,`concept_id`,`answer_concept`,`answer_drug`,`creator`,`date_created`,`uuid`,`sort_weight`) VALUES (9373,167570,664,NULL,1,'2026-04-15 10:15:20','00187095-3753-4a07-a10f-d443f2a47409',2);
 REPLACE INTO `concept_answer` (`concept_answer_id`,`concept_id`,`answer_concept`,`answer_drug`,`creator`,`date_created`,`uuid`,`sort_weight`) VALUES (9374,167570,703,NULL,1,'2026-04-15 10:15:20','7c63b92c-6b8e-4f5a-b8d9-455c95b23d47',1);
 
+REPLACE INTO `concept_answer` (`concept_answer_id`, `concept_id`, `answer_concept`, `answer_drug`, `creator`, `date_created`, `uuid`, `sort_weight`) VALUES (9375, 167614, 1065, NULL, 1, '2026-04-21 11:51:26', 'fb94f485-b2d3-405a-91a5-45b59adaabaf', 1);
+REPLACE INTO `concept_answer` (`concept_answer_id`, `concept_id`, `answer_concept`, `answer_drug`, `creator`, `date_created`, `uuid`, `sort_weight`) VALUES (9376, 167614, 1066, NULL, 1, '2026-04-21 11:51:26', 'a9b09243-0b29-448c-b7e3-87f9156b2cd7', 2);
+
+
 SET FOREIGN_KEY_CHECKS=1;
