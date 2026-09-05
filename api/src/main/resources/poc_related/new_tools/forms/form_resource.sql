5d2e74b13bd29c934173e605c332276e27d95d1d
Toyeeb <abdulfataitoyeeb@gmail.com>
2026-09-05 15:44:13 +0100

    update

--- api/src/main/resources/poc_related/new_tools/forms/form_resource.sql
+++ api/src/main/resources/poc_related/new_tools/forms/form_resource.sql
@@ -1,29 +1,5 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-DELETE FROM `form_resource` WHERE `form_resource_id` = 212;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1006;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1045;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1134;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1143;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1146;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1155;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1156;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1157;
-DELETE FROM `form_resource` WHERE `form_resource_id` = 1167;
 
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (877,69,'JSON schema','85a34570-174c-40cc-9893-db37b97909b7','AmpathJsonSchema',NULL,NULL,NULL,'9390e460-59c7-496a-9851-aef595bb3289',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1060,126,'JSON schema','b1a96b5b-7d59-4620-be69-6a1c0e4d2e1c','AmpathJsonSchema',NULL,NULL,NULL,'5054e5a8-5641-483f-9dd4-a23897f3596b',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1126,127,'JSON schema','8481ecf8-a341-4cba-b2a6-bcab27ee0823','AmpathJsonSchema',NULL,NULL,NULL,'a23834f7-6fc7-4f4f-a377-5a97b222e4a9',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1159,124,'JSON schema','f62dae77-78ae-4c5b-9132-7bd87e9673f1','AmpathJsonSchema',NULL,NULL,NULL,'31826f24-9d8b-4aed-95de-3878d9b6e900',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1161,131,'JSON schema','500208fa-770e-48aa-abd8-6eb44fb59517','AmpathJsonSchema',NULL,NULL,NULL,'39e27b8e-f99e-4ab5-b7ba-193c3a642131',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1180,132,'JSON schema','89126cf7-0cf0-4a3e-b671-3b1c2b90c7d5','AmpathJsonSchema',NULL,NULL,NULL,'8c284238-2862-46f2-a63d-927371fcd045',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1186,111,'JSON schema','89752c97-24dc-44c2-8f40-ba9b02b71417','AmpathJsonSchema',NULL,NULL,NULL,'c9712631-f363-4754-9f79-a1af6026a543',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1193,13,'JSON schema','eaa255f7-7f1e-4140-8c12-17f417553360','AmpathJsonSchema',NULL,NULL,NULL,'ab7bc459-7edc-492c-a88b-71b86c54ba57',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1198,122,'JSON schema','d4e0bfc8-e4c1-4df6-8a73-5ac7ed734b16','AmpathJsonSchema',NULL,NULL,NULL,'228ef7fe-3161-4cfb-98fd-6af5f6da81d2',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1214,128,'JSON schema','6635f135-1ad2-4f55-879b-5b4f489b4ef0','AmpathJsonSchema',NULL,NULL,NULL,'fd78c58f-fc90-4d81-9530-15c838e36f4d',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1235,14,'JSON schema','1713fe58-30ab-44bc-9660-def81ab48a60','AmpathJsonSchema',NULL,NULL,NULL,'f0639a9b-3adf-469e-8efc-961f70c7ce74',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1244,27,'JSON schema','4beb526e-9846-432a-adcc-4a07b95dd09b','AmpathJsonSchema',NULL,NULL,NULL,'3aa05058-0bcf-413e-9f2d-b9b837d2b111',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1245,139,'JSON schema','0cc6716d-2696-41a5-a979-2c109d14a29d','AmpathJsonSchema',NULL,NULL,NULL,'83da4815-cdfd-415a-9117-6b11833d3a38',NULL,NULL);
-REPLACE INTO `form_resource` (`form_resource_id`,`form_id`,`name`,`value_reference`,`datatype`,`datatype_config`,`preferred_handler`,`handler_config`,`uuid`,`date_changed`,`changed_by`) VALUES (1246,135,'JSON schema','560ac5e3-10fa-4df0-8cdf-be89320f417d','AmpathJsonSchema',NULL,NULL,NULL,'aa88c105-f92a-4252-93cf-bfe48a0bff71',NULL,NULL);
 
 SET FOREIGN_KEY_CHECKS=1;
